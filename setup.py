e573df22e8e4ec0bfe95a6389feac977f254f97d
DandyDrop <danilko223zaharov@gmail.com>
2023-08-24 23:05:23 +0300

    Test

--- setup.py
+++ setup.py
@@ -4,17 +4,17 @@ with open('README.md', 'r') as f:
     long_description = f.read()
 
 setuptools.setup(
-    name="flask-ngrok",
-    version="0.1.0",
-    author="DandyDrop",
-    description="A simple way to allow others access your Flask server using public url.",
+    name='pyp-template',
+    version='0.1.0',
+    author='DandyDrop',
+    description='A python package template',
     long_description=long_description,
-    long_description_content_type="text/markdown",
-    url="https://github.com/DandyDrop/pyp-template",
+    long_description_content_type='text/markdown',
+    url='https://github.com/DandyDrop/pyp-template',
     classifiers=[
-        "Programming Language :: Python :: 3.11",
-        "Operating System :: OS Independent",
+        'Programming Language :: Python :: 3.11',
+        'Operating System :: OS Independent',
     ],
-    keywords='flask ngrok expose public url',
+    keywords='python package template',
     install_requires=['Flask>=0.8', 'requests']
 )
