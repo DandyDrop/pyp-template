ccc5e5dd57c0ee9b76abf97441d322b2a5b1ecc7
DandyDrop <danilko223zaharov@gmail.com>
2023-08-24 23:21:58 +0300

    Test

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import setuptools
 with open('README.md', 'r') as f:
     long_description = f.read()
 
+with open('requirements-repl.txt', 'r') as f:
+    requirements = f.read().split('\n')
+
 setuptools.setup(
     name='pyp-template',
     version='0.1.0',
@@ -15,5 +18,6 @@ setuptools.setup(
         'Programming Language :: Python :: 3.11',
         'Operating System :: OS Independent',
     ],
-    keywords='python package template'
+    keywords='python package template',
+    install_requires=requirements
 )
